46c6095e3878a4b1ea1239e34c0b84dab9afba66
Loi <lonx90@gmail.com>
2017-03-11 11:27:55 +0700

    Dislaying data

--- displaying-data/README.md
+++ displaying-data/README.md
@@ -1,6 +1,6 @@
 # DisplayingData
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 1.0.0-rc.1.
+Property binding helps show app data in the UI.
 
 ## Development server
 Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The app will automatically reload if you change any of the source files.
--- displaying-data/src/app/app.component.ts
+++ displaying-data/src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
-
+import { Hero } from './model/hero';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  title: string;
+  myHero: Hero;
+  heroes = [
+    new Hero(1, 'Windstorm'),
+    new Hero(13, 'Bombasto'),
+    new Hero(15, 'Magneta'),
+    new Hero(20, 'Tornado')
+  ];
+  constructor() {
+    this.title = 'Tour of Heroes';
+    this.myHero = this.heroes[0];
+  }
 }
